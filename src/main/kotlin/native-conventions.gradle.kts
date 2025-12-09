6b96e275914614fd95ff62116fbb3896eedaacae
viatoro <watchkornnan.wiboonchart@krungsri.com>
2025-12-09 14:53:06 +0700

    init test

--- src/main/kotlin/native-conventions.gradle.kts
+++ src/main/kotlin/native-conventions.gradle.kts
@@ -2,3 +2,18 @@ plugins {
     id("java-conventions")
     id("org.graalvm.buildtools.native")
 }
+
+val libs = extensions.getByType<VersionCatalogsExtension>().named("libs")
+
+graalvmNative {
+    toolchainDetection.set(true)
+
+    binaries.configureEach {
+        javaLauncher.set(javaToolchains.launcherFor {
+            languageVersion.set(
+                JavaLanguageVersion.of(libs.findVersion("jdk").get().toString())
+            )
+            vendor.set(JvmVendorSpec.matching("GraalVM"))
+        })
+    }
+}
